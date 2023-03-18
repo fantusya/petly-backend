356c400389abd40df8ac026b943a111d6746e235
Anastasiia <guba.anastaysha@gmail.com>
2023-03-18 11:17:21 +0200

    updated controller for updating avatars

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,26 +1,61 @@
-const { User } = require("../../models/user");
-const path = require("path");
-const fs = require("fs/promises");
-const Jimp = require("jimp");
+const dotenv = require("dotenv");
+dotenv.config();
+const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
 
-const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+const { User } = require("../../models/user");
+// const path = require("path");
+const fs = require("fs/promises");
+// const Jimp = require("jimp");
+
+// const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET,
+});
 
 const updateAvatar = async (req, res) => {
-  const { path: tempUpload, originalname } = req.file;
+  const { path: tmpUpload } = req.file;
   const { _id: id } = req.user;
-  const imageName = `${id}_${originalname}`;
+
   try {
-    const resultUpload = path.join(avatarsDir, imageName);
-    Jimp.read(tempUpload, (err, image) => {
-      if (err) throw err;
-      image.resize(250, 250).write(avatarURL);
-    });
-    await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("public", "avatars", imageName);
-    await User.findByIdAndUpdate(req.user._id, { avatarURL });
-    res.json(avatarURL);
+    const result = await cloudinary.uploader.upload(
+      tmpUpload,
+      {
+        folder: "userAvatars",
+        transformation: [
+          { gravity: "face", height: 500, width: 500, crop: "crop" },
+          { radius: "max" },
+          { width: 60, height: 60, crop: "scale" },
+        ],
+      },
+      (error, result) => {
+        console.log("upload error", error);
+        return result;
+      }
+    );
+
+    const user = await User.findOne({ _id: id });
+    if (user.idCloudAvatar) {
+      await cloudinary.uploader.destroy(user.idCloudAvatar, (error, result) => {
+        console.log("destroy error", error);
+      });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      { _id: id },
+      {
+        avatarURL: result.secure_url,
+        idCloudAvatar: result.public_id,
+      },
+      { new: true }
+    );
+    await fs.unlink(tmpUpload);
+
+    res.json({ avatarURL: updatedUser.avatarURL });
   } catch (error) {
-    await fs.unlink(tempUpload);
+    await fs.unlink(tmpUpload);
     throw error;
   }
 };
