37c8e84385eb973a806a4ce7c1e0c42719a5e668
Anastasiia <guba.anastaysha@gmail.com>
2023-03-19 00:24:34 +0200

    added pets router

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 
 const usersRouter = require("./routes/api/users");
 const newsRouter = require("./routes/api/news");
+const petsRouter = require("./routes/api/pets");
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ app.use(express.json());
 
 app.use("/api/users", usersRouter);
 app.use("/api/news", newsRouter);
+app.use("/api/pets", petsRouter);
+
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
