e06a6b23f4bcb773dcd0eb7cd9006f48c7a99573
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-03-19 22:34:20 +0200

    Update getCurrent.js

--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,15 +1,21 @@
 const { Pet } = require("../../models/pet");
 
 const getCurrent = async (req, res) => {
-  const user = req.user;
+  const { _id, name, email, city, phone, birthDate, avatarURL } = req.user;
 
-  const myPets = await Pet.find({ owner: user._id }).populate(
+  const myPets = await Pet.find({ owner: _id }).populate(
     "owner",
-    "_id name email name city phone birthDate avatarURL token favoriteNotices"
+    "_id name email city phone birthDate avatarURLs"
   );
 
   res.json({
-    user,
+    _id,
+    name,
+    email,
+    city,
+    phone,
+    birthDate,
+    avatarURL,
     myPets,
   });
 };
